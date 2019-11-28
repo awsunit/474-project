244fda6afd29b0451c43ffd35a82b0106a427c2f
awsunit <38162688+awsunit@users.noreply.github.com>
2019-11-27 17:15:04 -0800

    Add files via upload

---
+++ main.c
@@ -0,0 +1,23 @@
+/*
+/
+  Copyright 2019
+  Authors:
+   DJ Wadhwa
+   Todd Gilbert
+  Contact:
+   awsunit@uw.edu
+
+  This file contains the main function of Dispenser.c
+  for the CSE Lab 5 project
+
+/
+*/
+/**/
+#include <stdlib.h>
+#include <Dispenser.h>
+int main (int argc, char **argv) {
+  
+  return EXIT_SUCCESS;
+}
+/**/
+/*EOF*/
